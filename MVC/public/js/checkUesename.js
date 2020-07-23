7d416d09329f491fff5085724fab1f9bd5e6acac
Mạnh Huỳnh <nguyenmanhhuynh98@gmail.com>
2020-07-23 12:22:28 +0700

    ,

--- MVC/public/js/checkUesename.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#exampleInputUsername1").keyup(function () { 
-        var user = $(this).val();
-        $.post("Ajax/checkUsername",{un:user},function(data){
-            $("#check").html(data);
-        });
-    });
-    alert("this is notifi");
-});
